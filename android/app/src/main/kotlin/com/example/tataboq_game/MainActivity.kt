bf6a21f29ecdce0b763ddc5176380465ad474df3
Wahbi Baklouti <wahbibaklouti7@gmail.com>
2021-04-16 16:33:20 +0100

    upload app folder

---
+++ android/app/src/main/kotlin/com/example/tataboq_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tataboq_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tataboq_game/home_screen.dart';
+
+main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Tataboq App",
+      home: HomeScreen(),
+    );
+  }
+}
